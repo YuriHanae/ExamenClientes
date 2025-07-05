a45f15de409c9ccd0d43bb3c27c82a89f41cec6e
Luis Arroyo <luisandresarroyoherrera@gmail.com>
2025-07-04 18:49:51 -0600

    Agregar archivos de proyecto.

---
+++ ExamenClientes/My Project/AssemblyInfo.vb
@@ -0,0 +1,31 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' La información general sobre un ensamblado se controla mediante el siguiente 
+' conjunto de atributos. Cambie los valores de estos atributos para modificar la información
+' asociada con un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+<Assembly: AssemblyTitle("ExamenClientes")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("ExamenClientes")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'El siguiente GUID es para el Id. typelib cuando este proyecto esté expuesto a COM
+<Assembly: Guid("8eac7224-eb63-42ce-b5d5-9e81f9f514f2")>
+
+' La información de versión de un ensamblado consta de los siguientes cuatro valores:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
