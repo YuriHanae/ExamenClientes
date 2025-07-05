31a95629289592a834a60c0a25cca3b1046f4bce
Luis Arroyo <luisandresarroyoherrera@gmail.com>
2025-07-04 19:11:40 -0600

    Añadida clase Cliente y referencia en el proyecto
    Se ha añadido el archivo `Cliente.vb` con la definición de la clase
    `Cliente`, que incluye propiedades públicas (`ClienteId`, `Nombre`,
    `Apellidos`, `Email`, `Telefono`) y un constructor para inicializarlas.
    También se ha actualizado el archivo `ExamenClientes.vbproj` para
    incluir la referencia a este nuevo archivo en el proyecto.

---
+++ ExamenClientes/Cliente.vb
@@ -0,0 +1,15 @@
+﻿Public Class Cliente
+    Public Property ClienteId As Integer
+    Public Property Nombre As String
+    Public Property Apellidos As String
+    Public Property Email As String
+    Public Property Telefono As String
+
+    Public Sub New(clienteId As Integer, nombre As String, apellidos As String, email As String, telefono As String)
+        Me.ClienteId = clienteId
+        Me.Nombre = nombre
+        Me.Apellidos = apellidos
+        Me.Email = email
+        Me.Telefono = telefono
+    End Sub
+End Class
